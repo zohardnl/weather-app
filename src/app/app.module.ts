9d4de7fcabb19607aca228a497413faaec232733
daniel zohar <daniel zohar@herolo.co.il>
2019-09-08 00:33:02 +0300

    start herolo-weather

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatMenuModule } from "@angular/material/menu";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		HttpClientModule,
+		MatButtonModule,
+		MatInputModule,
+		MatIconModule,
+		MatFormFieldModule,
+		FlexLayoutModule,
+		MatSnackBarModule,
+		MatMenuModule,
+		FormsModule,
+		ReactiveFormsModule,
+		MatDialogModule,
+		MatProgressSpinnerModule
+	],
+	entryComponents: [],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
