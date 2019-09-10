9b1e64276932f3f3512944bb0c5af2063bcaf4d9
daniel zohar <daniel zohar@herolo.co.il>
2019-09-10 21:01:31 +0300

    update base project

---
+++ src/app/weather-info/weather-info.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-weather-info",
+	templateUrl: "./weather-info.component.html",
+	styleUrls: ["./weather-info.component.scss"]
+})
+export class WeatherInfoComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
