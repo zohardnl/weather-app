5f4cf393424df0093f04a6eeb6ee047e5d0981db
daniel zohar <daniel zohar@herolo.co.il>
2021-02-09 23:38:58 +0200

    add hash for url

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,25 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { FavoritesComponent } from "./favorites/favorites.component";
-import { LoginComponent } from "./auth/login/login.component";
-import { SigupComponent } from "./auth/sigup/sigup.component";
-import { AuthGuard } from "./auth/auth.guard";
-import { WeatherDashboardComponent } from "./weather-dashboard/weather-dashboard.component";
-import { LoginGuard } from "./auth/login.guard";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SigupComponent } from './auth/sigup/sigup.component';
+import { AuthGuard } from './auth/auth.guard';
+import { WeatherDashboardComponent } from './weather-dashboard/weather-dashboard.component';
+import { LoginGuard } from './auth/login.guard';
 
 const routes: Routes = [
-	{ path: "", component: WeatherDashboardComponent, canActivate: [AuthGuard] },
-	{ path: "weather", component: WeatherDashboardComponent, canActivate: [AuthGuard] },
-	{ path: "signup", component: SigupComponent, canActivate: [LoginGuard] },
-	{ path: "login", component: LoginComponent, canActivate: [LoginGuard] },
-	{ path: "favorites", component: FavoritesComponent, canActivate: [AuthGuard] },
-	{ path: "**", redirectTo: "" }
+  { path: '', component: WeatherDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'weather', component: WeatherDashboardComponent, canActivate: [AuthGuard] },
+  { path: 'signup', component: SigupComponent, canActivate: [LoginGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [LoginGuard] },
+  { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
-	providers: [AuthGuard, LoginGuard]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
+  providers: [AuthGuard, LoginGuard]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule {
+}
